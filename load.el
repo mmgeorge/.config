07f0102b7de91f1cdf8ab879482d8bd162e172e5
matt <matt@rust>
2017-05-05 18:09:45 -0500

    .

---
+++ load.el
@@ -0,0 +1,9 @@
+
+(load "./emacs-visual.el")
+(load "./emacs-remove-unwanted-buffers.el")
+(load "./emacs-half-page-scroll.el")
+(load "./emacs-commands.el")
+(load "./emacs-native-modes.el")
+(load "./emacs-root.el")
+(load "./xemacs-templates.el")
+(load "./emacs-packages.el")
